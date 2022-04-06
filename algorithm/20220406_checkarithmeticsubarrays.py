bee3951c4c3e657f09e4155d0e31c57ea317f135
wlgn8648 <wlgn8648@gmail.com>
2022-04-06 09:26:44 +0900

    20220406

---
+++ algorithm/20220406_checkarithmeticsubarrays.py
@@ -0,0 +1,13 @@
+from astroid import List
+
+
+def check_arithmetic_subarrays(nums, l, r):
+    answer = []
+    for i, j in zip(l, r):
+        temp = sorted(nums[i:j + 1])
+        answer.append(len(set([temp[x + 1] - temp[x] for x in range(len(temp) - 1)])) == 1)
+    return answer
+
+
+if __name__ == '__main__':
+    print(check_arithmetic_subarrays([4, 6, 5, 9, 3, 7], [0, 0, 2], [2, 3, 5]))
