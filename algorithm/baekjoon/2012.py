5879f98d7d25184de0b1f4c6c90f5aca06d8a8f6
wlgn8648 <wlgn8648@gmail.com>
2022-05-14 22:34:16 +0900

    20220514

---
+++ algorithm/baekjoon/2012.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+n = int(input())
+pre = [int(input()) for x in range(n)]
+
+pre.sort()
+
+answer = 0
+
+for i in range(n):
+    answer += abs(i + 1 - pre[i])
+
+print(answer)
