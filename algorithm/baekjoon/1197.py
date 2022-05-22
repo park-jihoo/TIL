b69da81d5b79b147b339ce68cd55fd96761ba1b0
wlgn8648 <wlgn8648@gmail.com>
2022-05-22 20:29:45 +0900

    20220522

---
+++ algorithm/baekjoon/1197.py
@@ -0,0 +1,32 @@
+import sys
+import heapq
+
+sys.stdin = open("input.txt", "r")
+# Prim Algorithm
+
+input = sys.stdin.readline
+V, E = map(int, input().split())
+
+visited = [False] * (V + 1)
+Elist = [[] for _ in range(V + 1)]
+heap = [[0, 1]]
+
+for i in range(E):
+    s, e, w = map(int, input().split())
+    Elist[s].append([w, e])
+    Elist[e].append([w, s])
+
+answer, cnt = 0, 0
+
+while heap:
+    if cnt == V:
+        break
+    w, s = heapq.heappop(heap)
+    if not visited[s]:
+        visited[s] = True
+        answer += w
+        cnt += 1
+        for i in Elist[s]:
+            heapq.heappush(heap, i)
+
+print(answer)
