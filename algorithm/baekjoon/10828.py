1ca677773b6eef9b7b2df442dac9be67236aed3e
wlgn8648 <wlgn8648@gmail.com>
2022-05-26 12:34:56 +0900

    20220528

---
+++ algorithm/baekjoon/10828.py
@@ -0,0 +1,25 @@
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+N = int(sys.stdin.readline())
+q = []
+
+for i in range(N):
+    x = sys.stdin.readline().split()
+    if len(x) == 2:
+        q.append(int(x[1]))
+    elif x[0] == "pop":
+        if q:
+            print(q.pop(-1))
+        else:
+            print("-1")
+    elif x[0] == "size":
+        print(len(q))
+    elif x[0] == "empty":
+        print(int(len(q) == 0))
+    elif x[0] == 'top':
+        if q:
+            print(q[-1])
+        else:
+            print("-1")
