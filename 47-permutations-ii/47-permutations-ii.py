ade4cb63cea8e16903ce0ff84eb87df4b416ef6d
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-10-10 07:22:07 +0900

    Delete 47-permutations-ii directory

--- 47-permutations-ii/47-permutations-ii.py
+++
@@ -1,7 +0,0 @@
-from itertools import permutations
-
-class Solution:
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        uniqueset = set(list(permutations(nums, len(nums))))
-        return [list(x) for x in uniqueset]
-        
